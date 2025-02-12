e91451e429041517a86cd82fe3a43eec1028ca06
Dat Le <datle1108@gmail.com>
2025-02-12 08:31:11 +0100

    Add layout for app

---
+++ src/components/app-footer.tsx
@@ -0,0 +1,9 @@
+export default function AppFooter() {
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; 2025 DatLe. All rights reserved.
+      </small>
+    </footer>
+  );
+}
