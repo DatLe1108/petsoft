cad21ab52b56fee14fb8c0d13d784696f4d0e7f3
Dat Le <datle1108@gmail.com>
2025-02-16 17:55:00 +0100

    Add petcontext

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -6,17 +6,7 @@ import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 import React from "react";
 
-export default async function DashboardPage() {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch pets");
-  }
-
-  const data = await response.json();
-
+export default function DashboardPage() {
   return (
     <main>
       <div className="flex justify-between items-center text-white py-8">
@@ -31,7 +21,7 @@ export default async function DashboardPage() {
 
         <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
           <ContentBlock>
-            <PetList pets={data} />
+            <PetList />
           </ContentBlock>
         </div>
 
