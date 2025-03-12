9e36dc7c59e59f0f192e14d68809e2d9e674d9ac
Dat Le <datle1108@gmail.com>
2025-03-12 13:52:24 +0700

    Customize common component

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,14 @@
+import ContentBlock from "@/components/content-block";
+import H1 from "@/components/h1";
 import React from "react";
 
 export default function Account() {
-  return <div>Account Page</div>;
+  return (
+    <main>
+      <H1 className="my-8 text-white">Your account</H1>
+      <ContentBlock className="h-[500px] flex items-center justify-center">
+        <p>Logged in as ...</p>
+      </ContentBlock>
+    </main>
+  );
 }
