9055b4f315d983a99402f0d54e0dfafe0224b8e4
Dat Le <datle1108@gmail.com>
2025-03-20 23:27:07 +0800

    Create sign up/ log in page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,19 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 import React from "react";
 
 export default function SignUpPage() {
-  return <div>SignUpPage</div>;
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Sign Up</H1>
+      <AuthForm />
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account?{" "}
+        <Link href="/login" className="font-medium">
+          Login
+        </Link>
+      </p>
+    </main>
+  );
 }
