8d9bf54651bc52deb0ce730bad046bbecb095f38
Dat Le <datle1108@gmail.com>
2025-04-03 04:16:59 +0200

    Add log out feature

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
+import SignOutBtn from "@/components/sign-out-btn";
+
 export default async function Account() {
   const session = await auth();
 
@@ -14,8 +16,9 @@ export default async function Account() {
   return (
     <main>
       <H1 className="my-8 text-white">Your account</H1>
-      <ContentBlock className="h-[500px] flex items-center justify-center">
+      <ContentBlock className="h-[500px] flex flex-col gap-3 items-center justify-center">
         <p>Logged in as {session?.user.email}</p>
+        <SignOutBtn />
       </ContentBlock>
     </main>
   );
